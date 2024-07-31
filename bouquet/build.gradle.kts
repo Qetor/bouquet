c2e6a3d131717da0c802acfbbb936d7cb88ce062
Qetor <qetor@tutanota.com>
2024-07-31 17:59:31 +0200

    ci: attempt to fix jitpack

--- bouquet/build.gradle.kts
+++ bouquet/build.gradle.kts
@@ -50,3 +50,16 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
 }
+
+afterEvaluate {
+    publishing {
+        repositories {
+            mavenLocal()
+            maven {
+                name = "jitpack"
+                url = uri("https://jitpack.io")
+            }
+
+        }
+    }
+}
